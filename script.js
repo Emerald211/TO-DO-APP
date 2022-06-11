a1e9e038015f59c57d8101321d58109ab6a247ed
Emerald211 <aderibigbeemmanuel2004@gmail.com>
2022-06-11 10:13:38 -1200

    To do App

---
+++ script.js
@@ -0,0 +1,150 @@
+function goToRegister() {
+    const url = "register.html"
+    window.open( url)
+}
+
+function hidePassword() {
+   const pw = document.querySelector("#password");
+   const togglePassword = document.querySelector('#togglePassword');
+
+    if( pw.type === "password") {
+        pw.type = "text"
+        togglePassword.classList.remove("fa-eye-slash")
+        togglePassword.classList.add("fa-eye");
+    } else {
+        pw.type = "password"
+
+        togglePassword.classList.remove("fa-eye")
+        togglePassword.classList.add("fa-eye-slash");
+    }
+
+    togglePassword.classList.toggle("far fa-eye")
+
+
+}
+
+
+function hidePass() {
+    const pw = document.querySelector("#pass");
+    const togglePass = document.querySelector('#togglePass');
+ 
+     if( pw.type === "password") {
+         pw.type = "text"
+         togglePass.classList.remove("fa-eye-slash")
+         togglePass.classList.add("fa-eye");
+     } else {
+         pw.type = "password"
+ 
+         togglePass.classList.remove("fa-eye")
+         togglePass.classList.add("fa-eye-slash");
+     }
+ 
+     togglePass.classList.toggle("far fa-eye")
+ 
+ 
+ }
+
+ const form = document.querySelector("#form");
+ const password1 = document.querySelector("#password");
+ const password2 = document.querySelector("#pass");
+ const messageContainer = document.querySelector(".message-container");
+ const message = document.querySelector("#message");
+
+ let isValid = false;
+ let passwordMatch = false;
+
+ function validateForm() {
+     isValid = form.checkValidity();
+     console.log(isValid);
+
+     if(!isValid) {
+        message.textContent = "Fill out all details correctly";
+        message.style.color = "red";
+        messageContainer.style.border = "2px solid"
+        messageContainer.style.borderColor = "red";
+        return;
+     } 
+
+     if( password1.value === password2.value) {
+        passwordMatch = true;
+        message.textContent = "Registration Successful";
+        message.style.color = "green";
+        messageContainer.style.border = "2px solid"
+        messageContainer.style.borderColor = "green";
+     } else {
+         passwordMatch = false;
+         message.textContent = "Make sure password Match";
+         message.style.color = "red";
+         messageContainer.style.border = "2px solid"
+         messageContainer.style.borderColor = "red";
+         return;
+     }
+
+
+     if(isValid && passwordMatch) {
+        
+     message.textContent = "Registration Successful";
+     message.style.color = "green";
+     messageContainer.style.border = "2px solid"
+     messageContainer.style.borderColor = "green";
+
+     const url = "Todo.html"
+     window.open(url);
+     }
+
+
+
+ }
+
+
+
+
+
+ function processForm(a) {
+    a.preventDefault();
+
+    validateForm();
+    const user = {
+        name: form.fullname.value,
+        username: form.username.value,
+        email: form.email.value,
+        password: form.password.value,
+        confirmpassword: form.confirmpassword.value,
+        phone: form.phone.value
+    
+    };
+    if(isValid && passwordMatch) {
+        // storeData();
+        // console.log(user.name);
+
+        localStorage.setItem("name", user.name)
+        localStorage.setItem("password", user.password)
+
+        console.log(localStorage.getItem("name"));
+        console.log(localStorage.getItem("password"));
+    }
+   
+ }
+
+ function Login() {
+     const usernameLogin = document.querySelector("#usernameLogin");
+     const passwordLogin =  document.querySelector("#passwordLog");
+
+     console.log(usernameLogin.value);
+     console.log(passwordLogin);
+
+     if(usernameLogin.value === localStorage.getItem("name")  && passwordLogin.value === localStorage.getItem("password") ) {
+         alert("welcome")
+        const url = "Todo.html"
+         window.open(url)
+
+     } else {
+         alert (" no")
+     }
+ }
+
+ 
+
+
+
+ form.addEventListener('submit', processForm);
